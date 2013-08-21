db91af7b6827ccf8cbb8f4f64387ed449bdcd40a
NaPann <napan.np@gmail.com>
2013-08-21 08:51:34 +0700

    Third

---
+++ Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DisplayMeter
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                getLastUpdateLack();
+            }
+        }
+        private void getLastUpdateLack()
+        {
+            List<stp_get_LastMeterResult> result = DisplayMeterClass.getLastMaster();
+            this.RadListView1.DataSource = result;
+        }
+    }
+    
+}
--- Meter.Master.cs
+++ Meter.Master.cs
@@ -11,7 +11,15 @@ namespace DisplayMeter
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                
+            }
         }
+        /// <summary>
+        /// Get LastUpdate Lack Meter
+        /// </summary>
+       
+
     }
 }
